344bb02a77ade42d11c297f0edb49955f4c7d407
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-08 11:42:57 -0300

    Add- Chamei em javascript o html

---
+++ script.js
@@ -0,0 +1,12 @@
+function validacao() {
+
+    let nome = document.getElementById("nome").Value;
+    let email = document.getElementById("email").Value;
+    let idade = document.getElementById("idade").Value;
+    let peso = document.getElementById("peso").Value;
+    let tipo = document.getElementById("tipo").Value;
+    let telefone = document.getElementById("telefone").Value;
+    let cidade = document.getElementById("cidade").Value;
+    let estado = document.getElementById("estado").Value;
+
+}

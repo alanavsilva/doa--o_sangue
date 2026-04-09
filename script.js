bcdf4c73c1c86907de068426832fbd179dd75715
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-09 10:02:10 -0300

    Add- Arrumei alguns erros e adicionei mais validações

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
 function validacao() {
 
-    let nome = document.getElementById("nome").Value;
-    let email = document.getElementById("email").Value;
-    let idade = document.getElementById("idade").Value;
-    let peso = document.getElementById("peso").Value;
-    let tipo = document.getElementById("tipo").Value;
-    let telefone = document.getElementById("telefone").Value;
-    let cidade = document.getElementById("cidade").Value;
-    let estado = document.getElementById("estado").Value;
+    let valido = true;
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipo = document.getElementById("tipo").value;
+    let telefone = document.getElementById("telefone").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
 
     if (nome.length === 0) {
         alert("Informe seu nome")
+        valido = false;
+    }else if(!nome.includes(' ')){
+        alert("Deve conter nome e sobrenome")
+        valido = false;
     }
 
     if (email.length === 0) {
         alert("Informe seu email")
+        valido = false;
+    } else if (!email.includes("")) {
+        alert("Email deve possuir formato válido")
+        valido = false;
     }
 
     if (idade.length === 0) {
         alert("Informe sua idade")
+        valido = false;
     } else if (idade < 16) {
         alert("Deve ter no mínimo 16 anos para doar sangue")
+        valido = false;
     }
-    
+
     if (peso.length === 0) {
         alert("Informe seu peso")
+        valido = false;
     } else if (peso < 50) {
         alert("Deve ter no mínimo 50kg para doar sangue")
+        valido = false;
     }
 
     if (telefone.length === 0) {
         alert("Informe seu telefone")
+        valido = false;
+    }else if (isNaN(telefone)){
+       alert("Seu telefone deve conter apenas números") 
+       valido = false;
     }
 
     if (cidade.length === 0) {
         alert("Informe sua cidade")
+        valido = false;
     }
 
     if (estado.length === 0) {
         alert("Informe seu estado")
+        valido = false;
     }
 }
